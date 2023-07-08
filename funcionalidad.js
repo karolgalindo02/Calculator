480566c1d51af8032979adedbcfc69750264f840
wkarol <122057880+karolgalindo02@users.noreply.github.com>
2023-07-07 22:06:56 -0500

    Update funcionalidad.js

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,127 +1,12 @@
-/*
-//Declaramos variables 
-var operandoa;
-var operandob;
-var operacion;
-
-function init(){
-  //variables
-  var resultado = document.getElementById('resultado');
-  var reset = document.getElementById('reset');
-  var suma = document.getElementById('suma');
-  var resta = document.getElementById('resta');
-  var multiplicacion = document.getElementById('multiplicacion');
-  var division = document.getElementById('division');
-  var igual = document.getElementById('igual');
-  var uno = document.getElementById('uno');
-  var dos = document.getElementById('dos');
-  var tres = document.getElementById('tres');
-  var cuatro = document.getElementById('cuatro');
-  var cinco = document.getElementById('cinco');
-  var seis = document.getElementById('seis');
-  var siete = document.getElementById('siete');
-  var ocho = document.getElementById('ocho');
-  var nueve = document.getElementById('nueve');
-  var cero = document.getElementById('cero');
-}
-
-//Eventos de click
-  uno.onclick = function(e){
-      resultado.textContent = resultado.textContent  + "1";
-  }
-  dos.onclick = function(e){
-      resultado.textContent = resultado.textContent  + "2";
-  }
-  tres.onclick = function(e){
-      resultado.textContent = resultado.textContent  + "3";
-  }
-  cuatro.onclick = function(e){
-      resultado.textContent = resultado.textContent  + "4";
-  }
-  cinco.onclick = function(e){
-      resultado.textContent = resultado.textContent  + "5";
-  }
-  seis.onclick = function(e){
-      resultado.textContent = resultado.textContent  + "6";
-  }
-  siete.onclick = function(e){
-      resultado.textContent = resultado.textContent  + "7";
-  }
-  ocho.onclick = function(e){
-      resultado.textContent = resultado.textContent  + "8";
-  }
-  nueve.onclick = function(e){
-      resultado.textContent = resultado.textContent  + "9";
-  }
-  cero.onclick = function(e){
-      resultado.textContent = resultado.textContent  + "0";
-  }
-  reset.onclick = function(e){
-      resetear();
-  }
-  suma.onclick = function(e){
-      operandoa = resultado.textContent;
-      operacion = "+";
-      limpiar();
-  }
-  resta.onclick = function(e){
-      operandoa = resultado.textContent;
-      operacion = "-";
-      limpiar();
-  }
-  multiplicacion.onclick = function(e){
-      operandoa = resultado.textContent;
-      operacion = "*";
-      limpiar();
-  }
-  division.onclick = function(e){
-      operandoa = resultado.textContent;
-      operacion = "/";
-      limpiar();
-  }
-  igual.onclick = function(e){
-      operandob = resultado.textContent;
-      resolver();
-  }
-
-function limpiar(){
-  resultado.textContent = "";
-}
-
-function resetear(){
-  resultado.textContent = "";
-  operandoa = 0;
-  operandob = 0;
-  operacion = "";
-}
-
-function resolver(){
-  var res = 0;
-  switch(operacion){
-    case "+":
-      res = parseFloat(operandoa) + parseFloat(operandob);
-      break;
-
-    case "-":
-        res = parseFloat(operandoa) - parseFloat(operandob);
-        break;
-
-    case "*":
-      res = parseFloat(operandoa) * parseFloat(operandob);
-      break;
-
-    case "/":
-      res = parseFloat(operandoa) / parseFloat(operandob);
-      break;
-  }
-  resetear();
-  resultado.textContent = res;
-}
-*/
-const btnSwitch = document.querySelector("#switch");
+const btnSwitch = document.querySelector("#switch")
+const ENTER_KEY = "Enter"
+const DELETE_KEY = "Delete"
+const display = document.querySelector('.display') //selecting display
+const numbers = document.querySelectorAll('.number') //selecting all numbers
+const operators = document.querySelectorAll('.operator, .operator-start, .operator-end') //selecting all operators
 
 btnSwitch.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
+    document.body.classList.toggle('light');
     btnSwitch.classList.toggle('active');
 });
 
@@ -201,16 +86,11 @@ function back() {
 
 }
 
-//selecting display
-const display = document.querySelector('.display')
-//selecting all numbers
-const numbers = document.querySelectorAll('.number')
 //adding event listener for each number in "numbers"
 numbers.forEach( (button) => {
     button.addEventListener('click', calculate)
 })
-//selecting all operators
-const operators = document.querySelectorAll('.operator')
+
 //adding event listener for each operator in "operators"
 operators.forEach( (button) => {
     button.addEventListener('click', calculate)
@@ -230,9 +110,9 @@ function check(key) {
                     if(keyValue == operators[i].value) {
                         if (keyValue == "C") {
                             clean()
-                        } else if (keyValue == "⌫") {
+                        } else if (keyValue == "⌫" || keyValue == DELETE_KEY) {
                             back()
-                        } else if (keyValue == "=") {
+                        } else if (keyValue == "=" || keyValue == ENTER_KEY) {
                             equal()
                         } else {
                             display.value += keyValue
